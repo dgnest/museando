21f9d4c23467e9b08aebf009e53e52244eac8cf6
Oscar Giraldo Castillo <oscar.gi.cast@gmail.com>
2014-11-07 21:34:31 -0500

    add base files

---
+++ museando/museando/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for museando project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "museando.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
